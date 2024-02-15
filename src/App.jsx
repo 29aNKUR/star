67d2b084fbcff95138e6b2e79c67133716fd20af
Ankur Semle <semleankur@gmail.com>
2024-02-15 16:08:16 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import StarRating from './StarRating'
+
+function App() {
+
+  return (
+    <>
+      <div>
+      <StarRating limit={5}/>
+      </div>
+  
+    </>
+  )
+}
+
+export default App
---
+++ src/StarRating.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const StarRating = (props) => {
+
+    let limit = props.limit || 5;
+  return (
+    <div>
+        {console.log([...new Array(5).keys()])}
+    </div>
+  )
+}
+
+export default StarRating
